54c19eeaf25f1b8f10bbe770cedb3b3d63838646
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-01-22 10:50:58 -0500

    We made tests using mocha and chai

--- funcs.js
+++ funcs.js
@@ -1,13 +1,57 @@
 function isFive(num) {
   // Your code here
+  // We need to first test the typeof num
+    // if true, proceed
+    // if false, return false;
+
+  // next we need to test if num is === 5;
+    // if true, return true
+    // if false, return false
+
+  if (typeof num !== 'number') return false;
+  return (num === 5) ? true : false;
 }
 
 function isOdd(number) {
   // Your code here
+  /*
+  The first thing we need to test is check the type of the given number
+    if true; proceed
+    if false; throw new error
+
+  Next we are going to get the absolute value of said number to cover negative numbers
+  From there we will do modulo by 2 !== 0
+    if true; its odd
+    if false; its even
+  */
+
+  if(typeof number !== 'number') throw new Error();
+
+  const num = Math.abs(number);
+
+  return (number % 2 !== 0) ? true : false;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  /*
+  The first thing we need to test is if max is less than min
+    it true, return an empty array
+    it false, proceed
+
+  Next we will create an empty array
+  We are going to use a for-loop from min to including max
+    we are going to increment by step
+  */
+
+  const array = [];
+  if (min >= max) return array;
+
+  for (let i = min; i <= max; i += step) {
+    array.push(i);
+  }
+
+  return array;
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -6,29 +6,56 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const myWord = 'purpose';
+      const wordInstance = new Word(myWord);
+
+      expect(wordInstance.word).to.exist;
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const myWord = 'purpose';
+      const wordInstance = new Word(myWord);
+
+      expect(wordInstance.word).to.equal(myWord);
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const myWord = 'purpose';
+      const wordInstance = new Word(myWord);
+
+      const actual = wordInstance.removeVowels();
+      const expected = 'prps';
+
+      expect(actual).to.equal(expected);
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const myWord = 'purpose';
+      const wordInstance = new Word(myWord);
+
+      const actual = wordInstance.removeConsonants();
+      const expected = 'uoe';
+      expect(actual).to.equal(expected);
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const myWord = 'purpose';
+      const wordInstance = new Word(myWord);
+
+      const actual = wordInstance.pigLatin();
+      const expected = 'urposepay';
+      expect(actual).to.equal(expected);
     });
   });
 });
